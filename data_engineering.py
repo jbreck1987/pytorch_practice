5746a59f6fb084846de8a4f84becbd063a7819bf
jtb <bloo1987@proton.me>
2023-07-19 11:23:13 -0700

    add train/test/acc functions

--- data_engineering.py
+++ data_engineering.py
@@ -6,6 +6,9 @@ import numpy as np
 import numpy
 import math
 import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+import torch
+import random
 
 from mkidreadoutanalysis.quasiparticletimestream import QuasiparticleTimeStream
 from mkidreadoutanalysis.resonator import Resonator, ResonatorSweep, FrequencyGrid, RFElectronics, ReadoutPhotonResonator
@@ -136,4 +139,80 @@ def plot_stream_data(i_stream: numpy.array,
     ax[2].set_xlabel(f'Time ({units})')
     ax[2].set_ylabel('Photon Timestream', fontweight = 'bold', size = 'large')
     plt.show()
+
+
+def train_step(model: torch.nn.Module,
+               data_loader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer,
+               accuracy_fn) -> None:
+
+    # Training Steps
+    total_loss, acc = 0, 0 # need to reset loss every epoch
+    for batch, (X, y) in enumerate(data_loader): # each batch has 32 data/labels, create object -> (batch, (X, y))
+        model.train()
+        y_pred = model(X) # Like before, need to get model's predictions
+        loss = loss_fn(y_pred, y) # calculate loss for this batch
+        print(f'Batch loss: {loss.item()}')
+        total_loss += loss # add loss from this batch (mean loss of 32 samples) to total loss for the epoch (sum of all batch loss)
+        acc += accuracy_fn(y_pred, y)
+
+        # backprop step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # We want to see some updates within an epoch
+        print(f'Batches processed: {batch + 1}/{len(data_loader)}, Samples processed: {(batch + 1) * data_loader.batch_size}/{len(data_loader.dataset)}', end='\r')
+    
+    # Now we want to find the AVERAGE loss and accuracy of all the batches
+    total_loss /= len(data_loader)
+    acc /= len(data_loader)
+    print('\n-----------')
+    print(f'Mean Train Loss: {total_loss:.4f}, Mean Train Accuracy: {acc * 100:.2f}%')
+
+
+def test_step(model: torch.nn.Module,
+              data_loader: torch.utils.data.DataLoader,
+              loss_fn: torch.nn.Module,
+              accuracy_fn) -> None:
+
+    # Test Steps
+    model.eval()
+    total_loss, acc = 0, 0 # need to reset loss every epoch
+    with torch.inference_mode():
+        for X, y in data_loader: # each batch has 32 data/labels, create object -> (batch, (X_train, y_train))
+            y_pred = model(X) # Like before, need to get model's predictions
+            loss = loss_fn(y_pred, y) # calculate loss for this batch
+            total_loss += loss # add loss from this batch (mean loss of 32 samples) to total loss for the epoch (sum of all batch loss)
+            acc += accuracy_fn(y_pred, y)
+
+        # Now we want to find the AVERAGE loss and accuracy of all the batches
+        total_loss /= len(data_loader)
+        acc /= len(data_loader)
+    print(f'Mean Test Loss: {total_loss:.4f}, Mean Test Accuracy: {acc * 100:.2f}%')
+    print('-----------\n')
+
+
+def accuracy_fn(y_pred, y_true) -> float:
+    """Calculates accuracy between truth labels and predictions.
+
+    Args:
+        y_true (torch.Tensor): Truth labels for predictions.
+        y_pred (torch.Tensor): Predictions to be compared to predictions.
+
+    Returns:
+        [torch.float]: Accuracy value between y_true and y_pred, e.g. 78.45
+    """
+    return 1 - torch.nn.functional.mse_loss(target=y_true, input=y_pred).item()
+
+
+def make_predictions(model: torch.nn.Module, samples: list) -> torch.Tensor():
+    """
+    Given a list of samples, returns a tensor with the prediction for each sample
+    """
+    model.eval()
+    with torch.inference_mode():
+        preds_list = [model(x) for x in tqdm(samples, desc="Making predictions...")] 
+        return torch.cat(preds_list) # return as tensor instead of list
 
