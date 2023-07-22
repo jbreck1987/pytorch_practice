51f04aae5897494b48fc3c38d975eb661fa41ace
jtb <bloo1987@proton.me>
2023-07-21 17:35:30 -0700

    added single pulse functionality; removed accuracy_fn and logic

--- data_engineering.py
+++ data_engineering.py
@@ -49,10 +49,12 @@ def create_windows(i: numpy.array,
                    photon_arrivals: numpy.array,
                    with_pulses: list,
                    no_pulses: list,
+                   single_pulse: bool,
                    num_samples: int,
                    no_pulse_fraction: float,
                    edge_padding: int,
-                   window_size: int) -> None:
+                   window_size: int,
+                   ) -> None:
     """
     This function takes the output of the mkidreadoutanalysis objects (I, Q, and Photon Arrival vectors) and chunks it into smaller arrays. The code
     also separates chunks with photons and those without photons with the goal of limiting the number of samples
@@ -61,46 +63,51 @@ def create_windows(i: numpy.array,
     """
 
     # First determine the last index in the scanning range (need to have length of photon arrivals array be multiple of window_size)
-    end_index = math.floor(len(photon_arrivals) / window_size) * window_size
-
-    # Determine all the indicies in the i,q, photon_arrvial vecs denoting a pulse
-    pulse_indices = np.argwhere(photon_arrivals == 1).squeeze()
+    end_idx = math.floor(len(photon_arrivals) / window_size) * window_size
 
     # Now scan across the photon arrival vector and look at windows of length window_size with and without photon events
-    for window in range(0, end_index - window_size + 1, window_size):
-        # Check to see if any of the pulse indices are in this window and
-        if np.sum((pulse_indices >= window + edge_padding) & (pulse_indices < window + window_size - edge_padding)) > 0:
+    for window in range(0, end_idx - window_size + 1, window_size):
+        window_pulses = np.sum(photon_arrivals[window : window + window_size] == 1)
+        window_pulse_idxs = np.argwhere(photon_arrivals[window : window + window_size])
+        valid_window_start = window + edge_padding
+        valid_window_end = window + window_size - edge_padding
+        valid_pulse = ((window_pulse_idxs > valid_window_start) & (window_pulse_idxs < valid_window_end)).all()
+
+        # If there are more than one pulses in the entire window and we only want single pulse data, skip this window
+        if window_pulses > 1 and single_pulse:
+            continue
+
+        # Now any window with a pulse is valid as long as the pulse(s) dont live in the edge padding area
+        elif window_pulses > 0 and valid_pulse:
             # If so add the window to the with_pulses container
             with_pulses.append(np.vstack((i[window : window + window_size],
                                           q[window : window + window_size],
                                           photon_arrivals[window : window + window_size])).reshape(3, window_size)) # Reshaping to get in nice form for CNN
+
         # If no pulses are in the window and the no-pulse fraction hasn't been met,
         # add to the no_pulses container
-        elif len(no_pulses) < num_samples * no_pulse_fraction:
+        elif len(no_pulses) < num_samples * no_pulse_fraction and window_pulses == 0:
             no_pulses.append(np.vstack((i[window : window + window_size],
                                         q[window : window + window_size],
                                         photon_arrivals[window : window + window_size])).reshape(3, window_size)) # Reshaping to get in nice form for CNN
- 
-    
-        # Continue to next window doing nothing since threshold for no_pulse samples has been met and the window lacks a pulse
-        else:
-            continue
 
 def make_dataset(qp_timestream: QuasiparticleTimeStream,
                  num_samples: int,
                  no_pulse_fraction: float,
                  with_pulses: list,
                  no_pulses: list,
+                 single_pulse: bool,
                  cps=500,
                  edge_padding=0,
-                 window_size=150,
+                 window_size=150
             ) -> None:
     # Generate the training set in the following format: [np.array([i,q,label]), ...] where i,q,label are all WINDOW_SIZE length arrays.
     # Each list element is a 3 x 1 x WINDOW_SIZE numpy array.
+    count = len(with_pulses)
     while len(with_pulses) < num_samples - (num_samples * no_pulse_fraction):
         # We want the training data to be varied, so lets use the Poisson sampled
         # gen_photon_arrivals method to change the photon flux per iteration
-        photon_arrivals = qp_timestream.gen_photon_arrivals(cps=cps)
+        photon_arrivals = qp_timestream.gen_photon_arrivals(cps=cps, seed=None)
         qp_timestream.populate_photons() # this is necessary for the resonator object in the gen_iq function
         I, Q = gen_iq(qp_timestream)
         create_windows(I,
@@ -108,19 +115,24 @@ def make_dataset(qp_timestream: QuasiparticleTimeStream,
                        photon_arrivals,
                        with_pulses,
                        no_pulses,
+                       single_pulse,
                        num_samples,
                        no_pulse_fraction,
                        window_size=window_size,
                        edge_padding=edge_padding)
+        # Give status update on number of samples with photons
+        if len(with_pulses) > count:
+            print(f'Num samples with photons: {len(with_pulses)}/{num_samples - (num_samples * no_pulse_fraction)}', end='\r')
+            count = len(with_pulses)
     print(f'Number of samples with pulses: {len(with_pulses)}')
     print(f'Number of samples without pulses: {len(no_pulses)}')
 
 
 def plot_stream_data(i_stream: numpy.array,
-                    q_stream: numpy.array,
-                    label_stream: numpy.array,
-                    units: str
-                    ) -> None:
+                     q_stream: numpy.array,
+                     label_stream: numpy.array,
+                     units: str
+                     ) -> None:
     """
     Plots the training data. Assumes training samples are I and Q timestreams
     and the labels are timestreams of photon events (based on the I/Q values).
@@ -144,18 +156,15 @@ def plot_stream_data(i_stream: numpy.array,
 def train_step(model: torch.nn.Module,
                data_loader: torch.utils.data.DataLoader,
                loss_fn: torch.nn.Module,
-               optimizer: torch.optim.Optimizer,
-               accuracy_fn) -> None:
+               optimizer: torch.optim.Optimizer) -> None:
 
     # Training Steps
-    total_loss, acc = 0, 0 # need to reset loss every epoch
+    total_loss = 0 # need to reset loss every epoch
     for batch, (X, y) in enumerate(data_loader): # each batch has 32 data/labels, create object -> (batch, (X, y))
         model.train()
         y_pred = model(X) # Like before, need to get model's predictions
         loss = loss_fn(y_pred, y) # calculate loss for this batch
-        print(f'Batch loss: {loss.item()}')
         total_loss += loss # add loss from this batch (mean loss of 32 samples) to total loss for the epoch (sum of all batch loss)
-        acc += accuracy_fn(y_pred, y)
 
         # backprop step
         optimizer.zero_grad()
@@ -167,52 +176,33 @@ def train_step(model: torch.nn.Module,
     
     # Now we want to find the AVERAGE loss and accuracy of all the batches
     total_loss /= len(data_loader)
-    acc /= len(data_loader)
     print('\n-----------')
-    print(f'Mean Train Loss: {total_loss:.4f}, Mean Train Accuracy: {acc * 100:.2f}%')
+    print(f'Mean Train Loss: {total_loss:.4f}')
 
 
 def test_step(model: torch.nn.Module,
               data_loader: torch.utils.data.DataLoader,
-              loss_fn: torch.nn.Module,
-              accuracy_fn) -> None:
+              loss_fn: torch.nn.Module) -> None:
 
     # Test Steps
     model.eval()
-    total_loss, acc = 0, 0 # need to reset loss every epoch
+    total_loss = 0 # need to reset loss every epoch
     with torch.inference_mode():
         for X, y in data_loader: # each batch has 32 data/labels, create object -> (batch, (X_train, y_train))
             y_pred = model(X) # Like before, need to get model's predictions
             loss = loss_fn(y_pred, y) # calculate loss for this batch
             total_loss += loss # add loss from this batch (mean loss of 32 samples) to total loss for the epoch (sum of all batch loss)
-            acc += accuracy_fn(y_pred, y)
 
         # Now we want to find the AVERAGE loss and accuracy of all the batches
         total_loss /= len(data_loader)
-        acc /= len(data_loader)
-    print(f'Mean Test Loss: {total_loss:.4f}, Mean Test Accuracy: {acc * 100:.2f}%')
+    print(f'Mean Test Loss: {total_loss:.4f}')
     print('-----------\n')
 
-
-def accuracy_fn(y_pred, y_true) -> float:
-    """Calculates accuracy between truth labels and predictions.
-
-    Args:
-        y_true (torch.Tensor): Truth labels for predictions.
-        y_pred (torch.Tensor): Predictions to be compared to predictions.
-
-    Returns:
-        [torch.float]: Accuracy value between y_true and y_pred, e.g. 78.45
-    """
-    return 1 - torch.nn.functional.mse_loss(target=y_true, input=y_pred).item()
-
-
-def make_predictions(model: torch.nn.Module, samples: list) -> torch.Tensor():
+def make_predictions(model: torch.nn.Module, samples: list) -> list:
     """
     Given a list of samples, returns a tensor with the prediction for each sample
     """
     model.eval()
     with torch.inference_mode():
-        preds_list = [model(x) for x in tqdm(samples, desc="Making predictions...")] 
-        return torch.cat(preds_list) # return as tensor instead of list
+        return [model(x) for x in samples]
 
